ad1a11803f904da4d0d537d0b17b138cfe428154
MarvinRF <marvinvca@outlook.com>
2025-12-14 19:37:30 -0300

    Feat: Folder structure: Everything that interacts with external technology goes in infra/ and everything that exposes an API goes in modules/.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
+import { HealthModule } from './modules/health/health.module';
+import { HealthController } from './modules/health/health.controller';
+import { OrdersModule } from './modules/orders/orders.module';
+import { OrdersController } from './modules/orders/orders.controller';
+import { RabbitmqModule } from './infra/rabbitmq/rabbitmq.module';
 import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [],
-  controllers: [],
+  imports: [HealthModule, OrdersModule, RabbitmqModule],
+  controllers: [HealthController, OrdersController],
   providers: [],
 })
 export class AppModule {}
---
+++ src/infra/rabbitmq/rabbitmq.module.ts
@@ -0,0 +1,9 @@
+import { RabbitmqService } from './rabbitmq.service';
+import { Module } from '@nestjs/common';
+
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [RabbitmqService],
+})
+export class RabbitmqModule {}
